7e507897215d33b85449df22fd1e21e5a8702126
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-09-03 17:13:44 +0530

    Added create_app() method to initialise app instance based on config
    create_app() takes config_class and initialises app instance based on specifications specified in the Config classes in config.py

--- core/__init__.py
+++ core/__init__.py
@@ -14,6 +14,18 @@ migrate = Migrate(app, db)
 app.test_client()
 
 
+def create_app(config_class):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    with app.app_context():
+        db.create_all()  # Create tables based on the current configuration (test or production)
+
+    return app
+
 # this is to enforce fk (not done by default in sqlite3)
 @event.listens_for(Engine, "connect")
 def _set_sqlite_pragma(dbapi_connection, connection_record):
@@ -21,3 +33,6 @@ def _set_sqlite_pragma(dbapi_connection, connection_record):
         cursor = dbapi_connection.cursor()
         cursor.execute("PRAGMA foreign_keys=ON;")
         cursor.close()
+    
+
+
